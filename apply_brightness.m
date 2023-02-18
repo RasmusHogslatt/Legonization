63e2e0422214fc2e30edc9c8e8922676d267780a
RasmusHogslatt <r.hogslatt@gmail.com>
2023-02-18 13:40:29 +0100

    Brightness adjustments

--- apply_brightness.m
+++ apply_brightness.m
@@ -7,12 +7,14 @@ final_image = reproduced_image;
 lab_final_image = rgb2lab(reproduced_image,"ColorSpace","srgb","WhitePoint","d65");
 lab_ref_img = rgb2lab(reference_image,"ColorSpace","srgb","WhitePoint","d65");
 mean_lab_value = mean(mean(lab_ref_img(:,:,1))) / 100;
-
+prev_l_value = 0.5;
+l_value = 0.5;
 for i=1:size(reproduced_image, 2)
     for j=1:size(reproduced_image, 1)
+        prev_l_value = lab_ref_img(j,i,1) / 100;
         l_value = lab_final_image(j,i,1) / 100;
 
-        brightness = (l_value - mean_lab_value(1));
+        brightness = (l_value - prev_l_value);
         final_image(j, i, :) = brighten(final_image(j, i, :), brightness*factor);
     end
 end
